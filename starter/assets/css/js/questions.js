4909f1e472fe1f6adc8309059b34b904d72a81dd
justin motsi <jmotsi7@gmail.com>
2023-01-24 00:09:33 +0000

    changed the questions variable to a object array with properties question,choices and correct answer.

--- starter/assets/css/js/questions.js
+++ starter/assets/css/js/questions.js
@@ -1,10 +1,52 @@
-let questions = ["What is the JavaScript equivalent of “Hello World”?",
-"Which of these is a valid JavaScript variable name?",
-"What is the correct way to write a JavaScript array?",
-"How do you define a function in JavaScript?",
-"What is the output of the following JavaScript code?",
-"What is the output of the following JavaScript code?",
-"What is the purpose of the Math.round() function?",
-"Which of these is a JavaScript looping statement?",
-"Which of these functions will convert a string to a number?", 
-"What is the output of the following JavaScript code?"]
+let questions = [
+    {
+      question: "What is the JavaScript equivalent of “Hello World”?",
+      choices: ["alert('Hello World!');","console.log('Hello World!');", "print('Hello World!');","document.write('Hello World!');"],
+      correctAnswer: "console.log('Hello World!');"
+    },
+    {
+      question: "Which of these is a valid JavaScript variable name?",
+      choices: ["2variable;","Variable%name;","$variable;","Variable_name;"],
+      correctAnswer: "Variable_name;"
+    },
+    {
+      question: "What is the correct way to write a JavaScript array?",
+      choices: ["myArray = (1,2,3);","myArray = 1 2 3;","myArray = [1,2,3];","myArray = '1,2,3';"],
+      correctAnswer: "myArray = [1,2,3];"
+    },
+    {
+      question: "How do you define a function in JavaScript?",
+      choices: ["function myFunction()","function:myFunction","myFunction()=function","var myFunction = function()"],
+      correctAnswer: "function myFunction()"
+    },
+    {
+      question: "What is the output of the following JavaScript code? let x = 5; x+=2; console.log(x);",
+      choices: ["10","7","5","2"],
+      correctAnswer: "7"
+    },
+    {
+      question: "What is the output of the following JavaScript code? let a = 4; a = a * 2; console.log(a);",
+      choices: ["8","2","4","16"],
+      correctAnswer: "16"
+    },
+    {
+      question: "What is the purpose of the Math.round() function?",
+      choices: ["To round up a number","To round down a number","To round a number to the nearest integer","To round a number to the nearest 10"],
+      correctAnswer: "To round a number to the nearest integer"
+    },
+    {
+      question: "Which of these is a JavaScript looping statement?",
+      choices: ["for","if","switch","break"],
+      correctAnswer: "for"
+    },
+    {
+      question: "Which of these functions will convert a string to a number?",
+      choices: ["parseInt()","parseFloat()","eval()","typeOf()"],
+      correctAnswer: "parseInt()"
+    },
+    {
+      question: "What is the output of the following JavaScript code? let y = 5; y-=2; console.log(y);",
+      choices: ["7","5","2","10"],
+      correctAnswer: "2"
+    }
+  ];
